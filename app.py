9481757761f3b1422a164339e10570f56002bc69
Пивоварова Алина <pivovarova_alina02@mail.ru>
2023-09-06 18:39:48 +0700

    Меню

--- app.py
+++ app.py
@@ -2,7 +2,29 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
+@app.route("/index")
 def start():
+    return """
+<!doctype html>
+<html>
+     <head>
+           <title>НГТУ, ФБ, Лабораторные работы</title>
+     </head>
+     <body>
+            <header>
+            НГТУ, ФБ, WEB-программирование, часть 2. Спиок лабораторных
+            </header>
+
+            <h1>web-сервер на flask</h1>
+
+            <footer>
+            &copy; Пивоварова Алина Евгеньевна, ФБИ-14, 3 курс, 2023
+            </footer>
+        </body>
+</html>
+"""
+@app.route("/lab1")
+def lab1():
     return """
 <!doctype html>
 <html>
@@ -15,6 +37,12 @@ def start():
             </header>
 
             <h1>web-сервер на flask</h1>
+            <menu>Первая лабораторная</menu>
+            <p>
+            Flask - фреймворк для создания веб-приложений на языке программирования Python, использующий набор инструментов
+            Werkzeug, а также шаблонизатор Jinja2. Относится к категории так называемых микрофреймворков минималистичных каркасов
+            веб-приложений, сознательно предоставляющих лишь самые базовые возможности.
+            </p>
 
             <footer>
             &copy; Алина Евгеньевна, ФБИ-14, 3 курс, 2023
@@ -22,4 +50,5 @@ def start():
         </body>
 </html>
 """
+
             
