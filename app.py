b3739c31bdc9799390f54f8f763b9db9353fd542
Пивоварова Алина <pivovarova_alina02@mail.ru>
2023-09-06 18:09:04 +0700

    Вывод страницы первой лабораторной

--- app.py
+++ app.py
@@ -3,4 +3,23 @@ app = Flask(__name__)
 
 @app.route("/")
 def start():
-    return "web-сервер на flask"
+    return """
+<!doctype html>
+<html>
+     <head>
+           <title>Пивоварова Алина Евгеньевна, лабораторная 1</title>
+     </head>
+     <body>
+            <header>
+            НГТУ, ФБ, Лабораторная работа 1
+            </header>
+
+            <h1>web-сервер на flask</h1>
+
+            <footer>
+            &copy; Алина Евгеньевна, ФБИ-14, 3 курс, 2023
+            </footer>
+        </body>
+</html>
+"""
+            
