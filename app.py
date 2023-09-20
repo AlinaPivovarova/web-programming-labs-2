0cce1c1f84d67cce4933c4e8c5425af20f1df606
Алина Пивоварова <pivovarova_alina02@mail.ru>
2023-09-20 18:55:20 +0700

    Книги

--- app.py
+++ app.py
@@ -48,7 +48,7 @@ def lab1():
             Werkzeug, а также шаблонизатор Jinja2. Относится к категории так называемых микрофреймворков минималистичных каркасов
             веб-приложений, сознательно предоставляющих лишь самые базовые возможности.
             </p>
-            <a href="/menu" target="_blank" >Меню</a>
+            <a href="/menu">Меню</a>
             <h2>Реализованные роуты</h2>
             <ul>
                 <li>
@@ -185,4 +185,26 @@ def example():
     laba2 = 'Лабораторная работа 2'
     group = 'ФБИ-14'
     kurs = '3 курс'
-    return render_template('example.html', name=name, laba2=laba2, group=group, kurs=kurs)
+    student = 'Пивоварова Алина.'
+    fruits = [
+        {'name': 'яблоки', 'price': 100},
+        {'name': 'груши',  'price': 120},
+        {'name': 'апельсины', 'price': 80}, 
+        {'name': 'мандарины', 'price': 95},
+        {'name': 'манго', 'price': 321}
+        ]
+    books = [
+        {'author': 'Толстой Л.Н.', 'nazv': 'Анна Каренина', 'zanr': 'Классика', 'str': '159'},
+        {'author': 'Джейн Остин', 'nazv': 'Гордость и предубеждение', 'zanr': 'Классика', 'str': '423'},
+        {'author': 'Патрик Ленсиони', 'nazv': 'Евгений Онегин', 'zanr': 'Классика', 'str': '542'},
+        {'author': 'Джек Лондон', 'nazv': 'Мартин Иден', 'zanr': 'Классика', 'str': '600'},
+        {'author': 'Уильям Шекспир', 'nazv': 'Ромео и джульетта', 'zanr': 'Классика', 'str': '450'},
+        {'author': 'Станислав Лем', 'nazv': 'Солярис', 'zanr': 'Фантастика', 'str': '320'},
+        {'author': 'Фрэнк Герберт', 'nazv': 'Дюна', 'zanr': 'Фантастика', 'str': '400'},
+        {'author': 'Дэн Симмонс', 'nazv': 'Гиперион', 'zanr': 'Фантастика', 'str': '243'},
+        {'author': 'Нил Стивенсон', 'nazv': 'Анафем', 'zanr': 'Фантастика', 'str': '156'},
+        {'author': 'Айзек Азимов', 'nazv': 'Конец вечности', 'zanr': 'Фантастика', 'str': '356'}
+        ]
+    return render_template('example.html', 
+                            name=name, laba2=laba2, group=group,
+                            kurs=kurs, student=student, fruits=fruits, books=books)
