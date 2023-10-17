423b24b4fabf35fb419796fb46e81dde6c761e96
Пивоварова Алина <pivovarova_alina02@mail.ru>
2023-10-17 19:09:49 +0700

    Промежуточный этап

--- lab4.py
+++ lab4.py
@@ -1,7 +1,27 @@
 from flask import Blueprint, render_template, request
-lab4= Blueprint('lab4'__name__)
+lab4= Blueprint('lab4',__name__)
 
 
 @lab4.route('/lab4/')
 def lab():
     return render_template('lab4.html')
+
+
+@lab4.route('/lab4/uspeh')
+def uspeh():
+    return render_template('uspeh.html')
+
+
+@lab4.route('/lab4/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'GET':
+        return render_template("login.html")
+
+    username = request.form.get('username')
+    password = request.form.get('password')
+    
+    if username == 'alex' and password == '123':
+        return render_template('uspeh.html', username=username)
+
+    error = 'Неверный логин и/или пароль'
+    return render_template('login.html', error=error)
