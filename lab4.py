df5121ac557c4b51af6867a5f17b0b9281923611
Алина Пивоварова <pivovarova_alina02@mail.ru>
2023-10-24 10:49:01 +0700

    Холодильник начало

--- lab4.py
+++ lab4.py
@@ -31,3 +31,18 @@ def login():
     else:
         error = 'Неверный логин и/или пароль'
         return render_template('login.html', error=error, username=username, password=password)
+    
+
+@lab4.route('/lab4/holod', methods=['GET', 'POST'])
+def holod():
+    if request.method == 'GET':
+        return render_template('holod.html')
+    
+username = request.form.get('username')
+    if temperature == '':
+        error = 'ошибка: не задана температура'
+    if temperature > -12:
+        error = 'не удалось установить температуру — слишком низкое значение'
+    if temperature < -1:
+        error = 'не удалось установить температуру — слишком высокое значение'
+        return render_template('login.html', error=error, temperature=temperature)
