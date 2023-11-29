612fa52d8ea0599a70f0d880fcb90533fb1cad77
Пивоварова Алина <pivovarova_alina02@mail.ru>
2023-11-29 18:23:17 +0700

    Добавление в меню 7 лр

--- lab1.py
+++ lab1.py
@@ -39,7 +39,9 @@ def menu():
              <ol>
              <a href="/lab6/" target="_blank" >Шестая лабораторная</a>
             </ol>
-
+             <ol>
+             <a href="/lab7/" target="_blank" >Седьмая лабораторная</a>
+            </ol>
             <footer>
             &copy; Пивоварова Алина Евгеньевна, ФБИ-14, 3 курс, 2023
             </footer>
