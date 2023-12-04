a35c845f174c6c51ee1f83b673af3e2f06e75617
Пивоварова Алина <pivovarova_alina02@mail.ru>
2023-12-04 16:09:42 +0700

    Возврат оплаты

--- lab7.py
+++ lab7.py
@@ -13,16 +13,30 @@ def drink():
     return render_template('lab7/drink.html', name=None)
 
 
-@lab7.route('/lab7/api', methods = ['POST'])
+paid = False
+
+@lab7.route('/lab7/api', methods=['POST'])
 def api():
     data = request.json
+    global paid  # Используем глобальную переменную paid
 
     if data['method'] == 'get-price':
         return get_price(data['params'])
-
+    
     if data['method'] == 'pay':
-        return pay(data['params'])
-
+        response = pay(data['params'])
+        if not response.get("error"):
+            paid = True  # Обновляем состояние оплаты после успешной оплаты
+        return response
+    
+    if data['method'] == 'refund':
+        if paid:  # Проверяем состояние оплаты
+            response = refund(data['params'])
+            paid = False  # Обновляем состояние оплаты после успешного возврата
+            return response
+        else:
+            return {"result": None, "error": "Оплата еще не выполнена"}
+    
     abort(400)
 
 def get_price(params):
@@ -61,3 +75,14 @@ def pay(params):
     price = params['price']  # Используем переданную цену
 
     return {"result": f'С карты {card_num} списано {price} руб', "error": None}
+
+def refund(params):
+    card_num = params['card']
+    if len(card_num) != 16 or not str(card_num).isdigit():
+        return {"result": None, "error": "Неверный номер карты"}
+
+    cvv = params['cvv']
+    if len(cvv) != 3 or not cvv.isdigit():
+        return {"result": None, "error": "Неверный номер CVV/CVC"}
+
+    return {"result": f'Деньги возвращены на карту {card_num}', "error": None}
